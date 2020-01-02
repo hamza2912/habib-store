22d13484d0e44e8fbcfd8d168991f456ff2ef293
Syed Hamza Ali <sa02912@st.habib.edu.pk>
2020-01-02 13:01:30 +0500

    Habib store'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NabBarComponent } from './nab-bar/nab-bar.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductCartComponent } from './product-cart/product-cart.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NabBarComponent,
+    ProductsComponent,
+    ProductCartComponent,
+    NavBarComponent,
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nab-bar/nab-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nab-bar',
+  templateUrl: './nab-bar.component.html',
+  styleUrls: ['./nab-bar.component.css']
+})
+export class NabBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
